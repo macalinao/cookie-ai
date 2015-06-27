8d41e8dcadd986b225d44be2ef5e156f72aad5d1
Ian Macalinao <me@ian.pw>
2015-06-27 16:33:17 -0700

    Add session create on post

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from 'express';
+import routes from './lib/routes';
 
 let app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(routes());
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
