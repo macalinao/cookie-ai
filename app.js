27b450d9ac60c69b0b5eacbbc56dbbd78228dccb
pradeep <pradeep@pradeeps-MacBook-Pro.local>
2015-06-28 01:06:50 -0700

    test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let app = express();
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(routes());
 
