9981172212ddb521b6dfe101cdf5bc96130b08b2
pradeep <pradeep@pradeeps-MacBook-Pro.local>
2015-06-28 01:00:24 -0700

    test

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import routes from './lib/routes';
+import bodyParser from 'body-parser';
+
 
 let app = express();
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use(routes());
 
 let port = process.env.PORT || 3000;
